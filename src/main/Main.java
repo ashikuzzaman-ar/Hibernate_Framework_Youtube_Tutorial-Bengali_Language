9b5aeb1b3ee5e1b6d0aedbf49866628c12b6f9d6
ashik <ashik@studevs>
2016-08-20 03:59:28 +0600

    Lecture-6: Retrieving data from database using HQL.

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,6 +2,8 @@ package main;
 
 import db.config.SessionFactoryProvider;
 import db.models.UserInfo;
+import java.util.List;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
@@ -9,41 +11,33 @@ import org.hibernate.Transaction;
  *
  * @author ashik
  */
-public class Main implements SessionFactoryProvider{
+public class Main implements SessionFactoryProvider {
 
-    
     private static Session session;
     private static Transaction transaction;
+    private static Query query;
+    private static String hql;
+    private static List<UserInfo> userInfos;
 
     public static void main(String[] args) {
         // TODO code application logic here
 
-        UserInfo userInfo1 = new UserInfo();
-        UserInfo userInfo2 = new UserInfo();
-        userInfo1.setUsername("test_username_1");
-        userInfo1.setPassword("test_password_1");
-        userInfo1.setEmail("test_email_1");
-        userInfo2.setUsername("test_username_2");
-        userInfo2.setPassword("test_password_2");
-        userInfo2.setEmail("test_email_2");
-
         session = SESSION_FACTORY.openSession();
 
         try {
-            
-            transaction = session.beginTransaction();
-            
-            session.save(userInfo1);
-            session.save(userInfo2);
 
-            userInfo2.setUsername("Changed Username 2");
-            
+            transaction = session.beginTransaction();
+
+            hql = "FROM UserInfo UI WHERE UI.username='Changed Username 2'";
+
+            query = session.createQuery(hql);
+
+            userInfos = query.list();
+
             transaction.commit();
-            
-            userInfo1.setUsername("Changed Username 1");
         } catch (Exception e) {
 
-            if(transaction != null){
+            if (transaction != null) {
 
                 transaction.rollback();
             }
@@ -51,6 +45,17 @@ public class Main implements SessionFactoryProvider{
         } finally {
 
             session.close();
+        }
+
+        if (userInfos != null && userInfos.size() > 0) {
+
+            for (UserInfo userInfo : userInfos) {
+
+                System.out.println("ID: " + userInfo.getId());
+                System.out.println("IUsername: " + userInfo.getUsername());
+                System.out.println("Password: " + userInfo.getPassword());
+                System.out.println("Email: " + userInfo.getEmail());
+            }
         }
 
         SESSION_FACTORY.close();
