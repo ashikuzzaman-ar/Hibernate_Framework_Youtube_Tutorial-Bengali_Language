f8e266bd328a06541c5d4469a9132aced1ab8b2c
ashik <ashik@studevs>
2016-08-20 04:01:19 +0600

    Lecture-6: Retrieving data from database using Entity class and primary key.

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,8 +2,6 @@ package main;
 
 import db.config.SessionFactoryProvider;
 import db.models.UserInfo;
-import java.util.List;
-import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
@@ -15,12 +13,11 @@ public class Main implements SessionFactoryProvider {
 
     private static Session session;
     private static Transaction transaction;
-    private static Query query;
-    private static String hql;
-    private static List<UserInfo> userInfos;
 
     public static void main(String[] args) {
         // TODO code application logic here
+
+        UserInfo userInfo;
 
         session = SESSION_FACTORY.openSession();
 
@@ -28,11 +25,15 @@ public class Main implements SessionFactoryProvider {
 
             transaction = session.beginTransaction();
 
-            hql = "FROM UserInfo UI WHERE UI.username='Changed Username 2'";
+            userInfo = (UserInfo) session.get(UserInfo.class, 5L);
 
-            query = session.createQuery(hql);
+            if (userInfo != null) {
 
-            userInfos = query.list();
+                System.out.println("ID: " + userInfo.getId());
+                System.out.println("Username: " + userInfo.getUsername());
+                System.out.println("Password: " + userInfo.getPassword());
+                System.out.println("Email: " + userInfo.getEmail());
+            }
 
             transaction.commit();
         } catch (Exception e) {
@@ -45,17 +46,6 @@ public class Main implements SessionFactoryProvider {
         } finally {
 
             session.close();
-        }
-
-        if (userInfos != null && userInfos.size() > 0) {
-
-            for (UserInfo userInfo : userInfos) {
-
-                System.out.println("ID: " + userInfo.getId());
-                System.out.println("IUsername: " + userInfo.getUsername());
-                System.out.println("Password: " + userInfo.getPassword());
-                System.out.println("Email: " + userInfo.getEmail());
-            }
         }
 
         SESSION_FACTORY.close();
