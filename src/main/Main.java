bfb1e6c9fc13d8d35febc3f6a80a88ee9108db82
ashik <ashik@studevs>
2016-08-21 04:10:22 +0600

    Lecture-8: Delete an existing data after getting the data from database using HQL.

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,44 +12,41 @@ import org.hibernate.Transaction;
  * @author ashik
  */
 public class Main implements SessionFactoryProvider {
-
+    
     private static Session session;
     private static Transaction transaction;
-
+    
     public static void main(String[] args) {
         // TODO code application logic here
 
         UserInfo userInfo;
-
+        
         session = SESSION_FACTORY.openSession();
-
+        
         try {
-
+            
             transaction = session.beginTransaction();
-
-            String hql = "FROM UserInfo UI WHERE UI.username LIKE 'test_username_2%'";
+            
+            String hql = "FROM UserInfo UI WHERE UI.username = 'test_username_2'";
 //            userInfo = (UserInfo) session.get(UserInfo.class, 8L);
 //            userInfo = new UserInfo();
 //            userInfo.setId(10L);
 
             Query query = session.createQuery(hql);
             List<UserInfo> userInfos = query.list();
-
-            for(UserInfo ui: userInfos){
-                
-                session.delete(ui);
-            }
-
+            
+            session.delete(userInfos.get(0));
+            
             transaction.commit();
         } catch (Exception e) {
-
+            
             if (transaction != null) {
-
+                
                 transaction.rollback();
             }
             throw new ExceptionInInitializerError(e);
         } finally {
-
+            
             session.close();
         }
 
@@ -60,7 +57,6 @@ public class Main implements SessionFactoryProvider {
 //            System.out.println("Password: " + userInfo.getPassword());
 //            System.out.println("Email: " + userInfo.getEmail());
 //        }
-
         SESSION_FACTORY.close();
     }
 }
