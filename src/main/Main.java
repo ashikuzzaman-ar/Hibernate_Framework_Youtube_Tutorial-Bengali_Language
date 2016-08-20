89812f288054617d82b271aa6cf904a3d8dd221c
ashik <ashik@studevs>
2016-08-21 04:11:41 +0600

    Lecture-8: Delete an existing data after getting the data from database using Entity class and primary key.

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,8 +2,6 @@ package main;
 
 import db.config.SessionFactoryProvider;
 import db.models.UserInfo;
-import java.util.List;
-import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
@@ -12,41 +10,37 @@ import org.hibernate.Transaction;
  * @author ashik
  */
 public class Main implements SessionFactoryProvider {
-    
+
     private static Session session;
     private static Transaction transaction;
-    
+
     public static void main(String[] args) {
         // TODO code application logic here
 
         UserInfo userInfo;
-        
+
         session = SESSION_FACTORY.openSession();
-        
+
         try {
-            
+
             transaction = session.beginTransaction();
-            
-            String hql = "FROM UserInfo UI WHERE UI.username = 'test_username_2'";
-//            userInfo = (UserInfo) session.get(UserInfo.class, 8L);
+
+            userInfo = (UserInfo) session.get(UserInfo.class, 8L);
 //            userInfo = new UserInfo();
 //            userInfo.setId(10L);
 
-            Query query = session.createQuery(hql);
-            List<UserInfo> userInfos = query.list();
-            
-            session.delete(userInfos.get(0));
-            
+            session.delete(userInfo);
+
             transaction.commit();
         } catch (Exception e) {
-            
+
             if (transaction != null) {
-                
+
                 transaction.rollback();
             }
             throw new ExceptionInInitializerError(e);
         } finally {
-            
+
             session.close();
         }
 
