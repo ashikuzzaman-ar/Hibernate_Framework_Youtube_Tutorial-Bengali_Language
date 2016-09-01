6bfc71f34c28781b01661c48b9243a13ee4c28f9
ashik <ashik@studevs>
2016-09-01 20:52:21 +0600

    Lecture-13 : Dropped older model classes and created two new model and inserted a large number of data in database to show how hibernate works with a huge number of data.

---
+++ src/db/models/UserAuthentication.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package db.models;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author ashik
+ */
+@Entity
+@Table(name = "USER_AUTHENTICATION")
+public class UserAuthentication implements Serializable {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "USERNAME", length = 50)
+    private String username;
+
+    @Column(name = "PASSWORD")
+    private String password;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn
+    private UserDetails userDetails;
+
+    public UserAuthentication() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserDetails getUserDetails() {
+        return userDetails;
+    }
+
+    public void setUserDetails(UserDetails userDetails) {
+        this.userDetails = userDetails;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 29 * hash + Objects.hashCode(this.id);
+        hash = 29 * hash + Objects.hashCode(this.username);
+        hash = 29 * hash + Objects.hashCode(this.password);
+        hash = 29 * hash + Objects.hashCode(this.userDetails);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final UserAuthentication other = (UserAuthentication) obj;
+        if (!Objects.equals(this.username, other.username)) {
+            return false;
+        }
+        if (!Objects.equals(this.password, other.password)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return Objects.equals(this.userDetails, other.userDetails);
+    }
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -2,10 +2,9 @@ package main;
 
 import db.config.SessionFactoryProvider;
 import static db.config.SessionFactoryProvider.SESSION_FACTORY;
-import db.models.Test;
-import db.models.UserInfo;
-import java.util.HashSet;
-import java.util.Set;
+import db.models.UserAuthentication;
+import db.models.UserDetails;
+import java.util.Date;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
@@ -19,53 +18,32 @@ public class Main implements SessionFactoryProvider {
     private static Transaction transaction;
 
     public static void main(String[] args) {
-        // TODO code application logic here
 
-//        UserInfo userInfo;
         session = SESSION_FACTORY.openSession();
 
         try {
 
             transaction = session.beginTransaction();
 
-            UserInfo userInfo = new UserInfo();
-            userInfo.setEmail("e_1");
-            userInfo.setPassword("p_1");
-            userInfo.setUsername("u_1");
+            for (int i = 1; i <= 1000000; i++) {
 
-            Set<Test> tests = new HashSet<>();
+                UserAuthentication ua = new UserAuthentication();
+                ua.setUsername("Username " + i);
+                ua.setPassword("Password " + i);
 
-            Test test = new Test();
-            test.setAge(12.5D);
-            test.setIsMale(Boolean.FALSE);
+                UserDetails ud = new UserDetails();
+                ud.setAddress("Adress " + i);
+                ud.setBirthDate(new Date().toString());
+                ud.setEmail("Email " + i);
+                ud.setFirstName("First Name " + i);
+                ud.setLastName("Last Name " + i);
+                ud.setPhone("01xxxxx" + i);
+                ud.setUserAuthentication(ua);
 
-            Test test1 = new Test();
-            test1.setAge(15.5D);
-            test1.setIsMale(Boolean.TRUE);
+                ua.setUserDetails(ud);
 
-            tests.add(test);
-            tests.add(test1);
-
-            userInfo.setTests(tests);
-
-            Set<Test> tests1 = new HashSet<>();
-
-            Test test2 = new Test();
-            test2.setAge(25.5D);
-            test2.setIsMale(Boolean.TRUE);
-
-            tests1.add(test);
-            tests1.add(test2);
-
-            UserInfo userInfo1 = new UserInfo();
-            userInfo1.setEmail("e_2");
-            userInfo1.setPassword("p_2");
-            userInfo1.setUsername("u_2");
-
-            userInfo1.setTests(tests1);
-
-            session.save(userInfo);
-            session.save(userInfo1);
+                session.save(ua);
+            }
 
             transaction.commit();
         } catch (Exception e) {
@@ -80,38 +58,25 @@ public class Main implements SessionFactoryProvider {
             session.close();
         }
 
-        session = SESSION_FACTORY.openSession();
-        transaction = null;
-
-        UserInfo ui = null;
-        try {
-
-            transaction = session.beginTransaction();
-
-            ui = (UserInfo) session.get(UserInfo.class, 1L);
-//            Test t = (Test) session.get(Test.class, 1L);
-
-            transaction.commit();
-        } catch (Exception e) {
-
-            if (transaction != null) {
-
-                transaction.rollback();
-            }
-        } finally {
-
-            session.close();
-        }
-
-        if (ui != null) {
-
-            System.out.println("UserInfo Username: " + ui.getUsername());
-            for (Test t : ui.getTests()) {
-
-                System.out.println("Test Age: " + t.getAge());
-            }
-        }
-
+//        session = SESSION_FACTORY.openSession();
+//        transaction = null;
+//
+//        try {
+//
+//            transaction = session.beginTransaction();
+//
+//
+//            transaction.commit();
+//        } catch (Exception e) {
+//
+//            if (transaction != null) {
+//
+//                transaction.rollback();
+//            }
+//        } finally {
+//
+//            session.close();
+//        }
         SESSION_FACTORY.close();
     }
 }
